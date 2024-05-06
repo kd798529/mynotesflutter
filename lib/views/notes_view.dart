68b4b32c230b3447f130f69532d0b48c4f648059
Kwaku Dapaah <kwakudapaah@Kwakus-Mac-mini.local>
2024-05-06 00:03:02 -0400

    added logout feature

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 enum MenuAction { logout }
 
@@ -17,7 +19,18 @@ class _NotesViewState extends State<NotesView> {
         title: const Text("Your Notes"),
         actions: [
           PopupMenuButton<MenuAction>(
-            onSelected: (value) {},
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil("/login/", (_) => false);
+                  }
+                  break;
+              }
+            },
             itemBuilder: (BuildContext context) {
               return [
                 const PopupMenuItem<MenuAction>(
@@ -30,4 +43,27 @@ class _NotesViewState extends State<NotesView> {
       body: const Center(child: Text("Welcome")),
     );
   }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Sign out"),
+          content: const Text("Are you sure you want to sign out?"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text("Cancel")),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text("Log out"))
+          ],
+        );
+      }).then((value) => value ?? false);
 }
